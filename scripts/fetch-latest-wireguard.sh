aedf5b4aed6bffefdf171c35f99a60af53bb3a6f
Ronax <44991437+ronaxdevil@users.noreply.github.com>
2019-07-03 09:52:46 +0530

    Net/WireGuard: update WireGuard version
    - For now use 0.0.20190601 version of WireGuard
    
    Signed-off-by: ronaxdevil <pratabidya.007@gmail.com

--- scripts/fetch-latest-wireguard.sh
+++ scripts/fetch-latest-wireguard.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 set -e
-USER_AGENT="WireGuard-AndroidROMBuild/0.2 ($(uname -a))"
-
-exec 9>.wireguard-fetch-lock
-flock -n 9 || exit 0
+USER_AGENT="WireGuard-AndroidROMBuild/0.1 ($(uname -a))"
 
 [[ $(( $(date +%s) - $(stat -c %Y "net/wireguard/.check" 2>/dev/null || echo 0) )) -gt 86400 ]] || exit 0
 
-while read -r distro package version _; do
-	if [[ $distro == upstream && $package == kmodtools ]]; then
-		VERSION="$version"
-		break
-	fi
-done < <(curl -A "$USER_AGENT" -LSs https://build.wireguard.com/distros.txt)
-
-[[ -n $VERSION ]]
-
-if [[ -f net/wireguard/version.h && $(< net/wireguard/version.h) == *$VERSION* ]]; then
-	touch net/wireguard/.check
-	exit 0
-fi
+[[ $(curl -A "$USER_AGENT" -LSs https://git.zx2c4.com/WireGuard/refs/) =~ snapshot/WireGuard-([0-9.]+)\.tar\.xz ]]
 
 rm -rf net/wireguard
 mkdir -p net/wireguard
-curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-$VERSION.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "WireGuard-$VERSION/src"
+curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-0.0.20190601.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "WireGuard-0.0.20190601/src"
 sed -i 's/tristate/bool/;s/default m/default y/;' net/wireguard/Kconfig
 touch net/wireguard/.check
